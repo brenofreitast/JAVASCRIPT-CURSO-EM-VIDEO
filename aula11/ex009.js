d5792d93c3fe78a8fb4c69df4551e7da7d5b5c7f
brenofreitast <brenofreitast@gmail.com>
2025-11-03 15:44:26 -0300

    create ex009 - condicao composta

---
+++ aula11/ex009.js
@@ -0,0 +1,10 @@
+// Para ultilizar a extensao Node.js Exec precionar a tecla "F8"
+
+var pais = "EUA" 
+
+console.log(`Vivendo em ${pais}`)
+if(pais=="BRA"){
+    console.log('Você é Brasileiro.')
+}else{
+    console.log("Você é estrangeiro.")
+}
