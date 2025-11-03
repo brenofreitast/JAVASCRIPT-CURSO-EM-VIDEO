274a015aaa1d62173c57acf14a0e9bb00940168f
brenofreitast <brenofreitast@gmail.com>
2025-11-03 15:39:47 -0300

    Create ex008 - Condicao simples

---
+++ aula11/ex008.js
@@ -0,0 +1,11 @@
+// Para ultilizar a extensao Node.js Exec precionar a tecla "F8"
+
+var vel = 60.5
+
+// Condição Simples - apenas 1 if, e sem else.
+if (vel>60){
+    console.log("Voce ultrapassou a velocidade permitida.")
+}
+
+//console.log(`A velocidade do seu carro é de ${vel}km/h`)
+//console.log("Tenha cuidado.")
