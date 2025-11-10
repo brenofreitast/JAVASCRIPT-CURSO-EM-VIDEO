17edd246d4dc9b4a667ae847434ab5e891421d04
brenofreitast <brenofreitast@gmail.com>
2025-11-10 14:25:13 -0300

    aula13 - While - Do While

---
+++ aula13/ambiente.js
@@ -0,0 +1,20 @@
+
+// DO WHILE ->
+var contador = 0
+
+do {
+    console.log(`Contador = ${contador}`)
+    contador++
+} while (contador<6)
+
+    
+
+
+/*  WHILE ->
+    var contador = 0
+
+    while(contador<6){
+        console.log(`Contador = ${contador}`)
+        contador++
+    }
+*/
