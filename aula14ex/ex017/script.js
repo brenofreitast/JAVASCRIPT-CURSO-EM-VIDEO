12f7488dabe020956ed30fd0bef5f1acffe2abf1
brenofreitast <brenofreitast@gmail.com>
2025-11-12 14:38:37 -0300

    ex17 - tabuada

--- aula14ex/ex017/script.js
+++ aula14ex/ex017/script.js
@@ -1,8 +1,28 @@
-var numero = document.getElementById('numero')
+const { createElement } = require("react")
 
-
-function gerar(){
+function gerar() {
+    var numero = document.getElementById('numero')
     var num = Number(numero.value)
+    var tabuada = document.getElementById('tabuada')
+    tabuada.innerHTML = ''
+    var ul = document.createElement('ul')
+
+
+    if (num == '') {
+        alert('Por favor, digite um número!')
+    } else {
+        for (i = 0; i <= 10; i++) {
+            var tab = num * i
+            var li = document.createElement('li')
+            li.textContent = `${num} x ${i}= ${tab}`
+            ul.appendChild(li)
+        }
+        tabuada.appendChild(ul)
+    }
+
+
+
+
 
 
 }
